2f8e6e29a1c4213627f84be5db4f743470ae5e13
SamDC97 <sam.decock97@gmail.com>
2021-01-16 16:23:36 +0100

    initial commit

---
+++ src/test/java/be/exam/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.exam.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
